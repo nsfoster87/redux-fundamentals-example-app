41e1f6ac05d6a90b3f49294fe665acfbe959c2e8
Nate Foster <nate.s.foster@gmail.com>
2022-09-08 12:54:45 -0400

    update unsaved reducer.js file

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import todosReducer from './features/todos/todosSlice';
 import filtersReducer from './features/filters/filtersSlice';
 
+// This is basically what the combineReducers() function does
 // export default function rootReducer(state = {}, action) {
 //   return {
 //     todos: todosReducer(state.todos, action),
